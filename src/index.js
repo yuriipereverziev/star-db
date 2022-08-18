dbeab3b46533b292c95793a2247f39e2d299d89e
yuriipereverziev <yuripereverzev@gmail.com>
2022-08-18 16:17:43 +0300

    add: structure

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
-class SwapiService {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-    _apiBase = 'https://swapi.dev/api';
-    async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}`,
-                +`received ${res.status}`)
-        }
-
-
-        return await res.json()
-    }
-
-    async getAllPeople() {
-        const res = await this.getResource(`/people/`)
-        return res.results
-    }
-    async getAllPerson(id) {
-        return this.getResource(`/people/${id}/`);
-    }
-}
-
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then((people)=>{
-    people.forEach((p)=>{
-        console.log(p.name)
-    })
-})
-
-
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
