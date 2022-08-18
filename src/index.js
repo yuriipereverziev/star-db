ee0e2270676cfbfdbac22679a9a208160c73b7ac
yuriipereverziev <yuripereverzev@gmail.com>
2022-08-18 16:25:35 +0300

    fix: ReactDOM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
